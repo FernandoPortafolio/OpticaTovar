cc9905a186d0c87e6338d1bbd06aa1156927b914
Fernando Acosta <Fernando Acosta>
2021-01-24 23:40:21 -0600

    pagina 404

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,13 @@
-const { Router } = require('express');
-const router = Router();
+const { Router } = require('express')
+const router = Router()
 
 //importing all routes
-router.use(require('./navigation.routes'));
-router.use('/api/products', require('./products.routes'));
+router.use(require('./navigation.routes'))
+router.use('/api/products', require('./products.routes'))
+router.use((req, res, next) => {
+  res.render('404', {
+    layout: '404',
+  })
+})
 
-module.exports = router;
+module.exports = router
